e839acec417eaacf0bd0bfe6febbddef0dd51d4b
onowoey <cg10046@naver.com>
2025-10-22 17:49:14 +0900

    Feat: 초록누리 api 응답 xml 파싱

--- mixsafe_backend/src/main/java/com/nemo/mixsafe/dto/Green/Green5ResponseDto.java
+++ mixsafe_backend/src/main/java/com/nemo/mixsafe/dto/Green/Green5ResponseDto.java
@@ -1,4 +1,28 @@
 package com.nemo.mixsafe.dto.Green;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.dataformat.xml.annotation.*;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JacksonXmlRootElement(localName = "rows")
 public class Green5ResponseDto {
+
+    @JacksonXmlProperty(localName = "count")
+    private Integer count;
+
+    @JacksonXmlProperty(localName = "resultcode")
+    private String resultcode;
+
+    @JacksonXmlProperty(localName = "pagenum")
+    private Integer pagenum;
+
+    @JacksonXmlProperty(localName = "pagesize")
+    private Integer pagesize;
+
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "row")
+    private List<Green5Row> rows;
 }
