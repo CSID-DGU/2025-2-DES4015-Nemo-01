7d7f4b81e0594e978c2b9c6c576765f964aabf04
onowoey <cg10046@naver.com>
2025-11-09 22:47:14 +0900

    Fix: Ai 응답 형식 변경

--- mixsafe_backend/src/main/java/com/nemo/mixsafe/dto/Ai/AiResponseDto.java
+++ mixsafe_backend/src/main/java/com/nemo/mixsafe/dto/Ai/AiResponseDto.java
@@ -1,16 +1,29 @@
 package com.nemo.mixsafe.dto.Ai;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 @Schema(description = "ai 응답 DTO")
 
 public class AiResponseDto {
 
-    @Schema(description = "ai 분석 결과")
-    private String Text;
+    @JsonProperty("success")
+    private Boolean success;
+
+    @JsonProperty("simple_response")
+    private SimpleResponse simpleResponse;
+
+    @Data
+    public static class SimpleResponse {
+        @JsonProperty("risk_level")
+        private String riskLevel;
+
+        @JsonProperty("message")
+        private String message;
+    }
 
 }
