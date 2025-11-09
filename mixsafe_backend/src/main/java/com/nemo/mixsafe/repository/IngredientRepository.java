1965a2882469300b2f16408c7053cf0546a1f127
onowoey <cg10046@naver.com>
2025-11-10 00:05:13 +0900

    Feat: Ai 연결 로직 구현

--- mixsafe_backend/src/main/java/com/nemo/mixsafe/repository/IngredientRepository.java
+++ mixsafe_backend/src/main/java/com/nemo/mixsafe/repository/IngredientRepository.java
@@ -5,9 +5,11 @@ import com.nemo.mixsafe.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
     boolean existsByProductAndCasNo(Product product, String casNo);
-
     Ingredient save(Ingredient ingredient);
+    List<Ingredient> findByProduct(Product product);
 }
